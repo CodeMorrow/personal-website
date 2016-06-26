e55f26ee8ee3aa6d9f96c9716111caab0e92195c
John Morrow <CodeMorrow@yahoo.com>
2016-06-26 12:35:02 -0500

    RPS folder and Pictures added

--- includes/nav.php
+++ includes/nav.php
@@ -5,25 +5,25 @@
 		<div class="tab is_tab_selected">
 			<?php if ($thisPage=="Home - My Website") 
 				echo "*"; ?>
-					<a href="index.php">Home</a>
+					<a href="http://CodeMorrow.omahacodeschool.com/index.php">Home</a>
 		</div>
 
 		<div class="tab">
 			<?php if ($thisPage=="Articles") 
 				echo "*"; ?>
-					<a href="articles.php">Articles</a>
+					<a href="http://CodeMorrow.omahacodeschool.com/articles.php">Articles</a>
 		</div>
 
 		<div class="tab">
 			<?php if ($thisPage=="Goals") 
 				echo "*"; ?>
-					<a href="goals.php">Goals</a>
+					<a href="http://CodeMorrow.omahacodeschool.com/goals.php">Goals</a>
 		</div>
 
 		<div class="tab">					
 			<?php if ($thisPage=="About Me") 
 				echo "*"; ?>
-					<a href="about_me.php">About Me</a>
+					<a href="http://CodeMorrow.omahacodeschool.com/about_me.php">About Me</a>
 		</div>			
 	</div>
 	
