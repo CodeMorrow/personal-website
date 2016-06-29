311812d3484842c1355b4b890d3198847bbaaa48
John Morrow <CodeMorrow@yahoo.com>
2016-06-29 16:05:17 -0500

    Designed TTT Board.

--- games/ttt/verses_computer.php
+++ games/ttt/verses_computer.php
@@ -10,22 +10,22 @@
 <br>
 <!-- Tic Tac Toe Board -->
 <div class="board ticTacToe">
-	<div class="ticTacToe__column">
-		<div class="ticTacToe__square"></div>
-		<div class="ticTacToe__square"></div>
-		<div class="ticTacToe__square"></div>
+	<div class="ticTacToe__row">
+		<div class="ticTacToe__square" id="box0">0</div>
+		<div class="ticTacToe__square" id="box1">1</div>
+		<div class="ticTacToe__square" id="box2">2</div>
 	</div>
 
-	<div class="ticTacToe__column">
-		<div class="ticTacToe__square"></div>
-		<div class="ticTacToe__square"></div>
-		<div class="ticTacToe__square"></div>
+	<div class="ticTacToe__row">
+		<div class="ticTacToe__square" id="box3">3</div>
+		<div class="ticTacToe__square" id="box4">4</div>
+		<div class="ticTacToe__square" id="box5">5</div>
 	</div>
 
-	<div class="ticTacToe__column">
-		<div class="ticTacToe__square"></div>
-		<div class="ticTacToe__square"></div>
-		<div class="ticTacToe__square"></div>
+	<div class="ticTacToe__row">
+		<div class="ticTacToe__square" id="box6">6</div>
+		<div class="ticTacToe__square" id="box7">7</div>
+		<div class="ticTacToe__square" id="box8">8</div>
 	</div>
 </div>
 
