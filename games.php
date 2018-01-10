423e36810d1d6e0da7608176b306c2c267f2e36a
John Morrow <CodeMorrow@yahoo.com>
2018-01-09 21:00:00 -0600

    open games in new windows

--- games.php
+++ games.php
@@ -5,10 +5,10 @@
 	<h3>Let's Play a Game!</h3>
 
 	<ul class="gameList">
-		<li> <a href="games/rps/index.php">Rock, Paper, Scissors</a>
+		<li> <a href="games/rps/index.php" target="_blank">Rock, Paper, Scissors</a>
 	<br>
 	<br>
-		<li> <a href="games/ttt/index.php">Tic-Tac-Toe</a></li>
+		<li> <a href="games/ttt/index.php" target="_blank">Tic-Tac-Toe</a></li>
 	</ul>
 
 <?php include 'includes/_bottom.php' ?>
